e38c8c4dcf19f1d89faa8cdd543e784584077a19
TheresaHHH <hsuanhuahung@ufl.edu>
2023-09-10 00:52:24 -0400

    replace server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require('http'), 
+var http = require('http'),
+    url = require('url'), 
     fs = require('fs'), 
     port = 8080;
 
@@ -9,8 +10,21 @@ var requestHandler = function(request, response) {
   /*Investigate the request object. 
     You will need to use several of its properties: url and method
   */
-  //console.log(request);
-
+//console.log(request);
+//var q= parsedUrl.query;
+//var parsedUrl = url.parse(request.url);
+ if (request.url === '/' || request.method !='GET' ) {
+    //console.log(request.method);
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end('Bad gateway error - path');
+} else if(request.url === '/listings')
+{
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end( JSON.stringify(listingData));
+}else{
+    response.writeHead(404);
+    response.end('Bad gateway error - path');
+}
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -24,11 +38,7 @@ var requestHandler = function(request, response) {
     
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-
-    */
+   */
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -43,18 +53,17 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
 
-    //Check for errors
-    /*this resource gives you an idea of the general format err objects and Throwing an existing object.
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
-   */
-  
+    //Check for errors 
+    if (err) throw err;
 
-   //Save the data in the listingData variable already defined
-  
-
+   //Save the state in the listingData variable already defined
+  //listingData= data;
+  listingData=JSON.parse(data); 
   //Creates the server
-  
+  server = http.createServer(requestHandler);
   //Start the server
-
-
+  server.listen(port, function() {
+  //once the server is listening, this callback function is executed
+  console.log('Server listening on: http://127.0.0.1:' + port);
+});
 });
